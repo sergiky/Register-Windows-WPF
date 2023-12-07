e39593dadcc36d03ceb9e18b1932ac26590bc955
Sergio Guijarro <90042979+sergiky@users.noreply.github.com>
2023-12-07 08:25:07 +0100

    Add files via upload

---
+++ LibraryCustomizedControls/CustomTextBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrayNotify;
+using System.Windows.Forms.VisualStyles;
+
+namespace LibraryCustomizedControls
+{
+    public partial class CustomTextBox : TextBox
+    {
+        public CustomTextBox()
+        {
+            InitializeComponent();
+
+            this.FontHeight = 12;
+            this.FontFamily = new FontFamily("Arial");
+
+        }
+    }
+}
---
+++ LibraryStateCheckbox/StateCheckbox.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LibraryStateCheckbox
+{
+    /// <summary>
+    /// Lógica de interacción para StateCheckbox.xaml
+    /// </summary>
+    public partial class StateCheckbox : UserControl
+    {
+        private double width = 0;
+        private double height = 0;
+        public StateCheckbox()
+        {
+            InitializeComponent();
+            updateState();
+        }
+
+
+        private void updateState()
+        {
+            chkState.Width = this.width;
+            chkState.Height = this.height;
+        }
+
+        public double Width1 { 
+            get => width;
+            set {
+                width = value;
+                updateState();
+            } 
+        }
+        public double Height1 {
+            get => height;
+            set 
+            {
+                height = value;
+                updateState();
+            } 
+        }
+
+
+    }
+}
